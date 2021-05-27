5ac9cee1defb23b4fc4b1c6cf22e58cae4a42f24
Daniel Murnane <murnanedaniel@hotmail.com>
2021-05-27 09:31:45 -0700

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Exploratory studies of Lorentz/Rotational equivariance and invariance',
+    author='Daniel Murnane (Exatrkx)',
+    license='MIT',
+)
